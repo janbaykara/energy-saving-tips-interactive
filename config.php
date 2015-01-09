8677c53d58c4bf0ce126f5ba67eebb18d3a5a988
Jan Baykara <jan@baykara.co.uk>
2015-01-09 09:37:56 +0000

    Metadata

--- config.php
+++ config.php
@@ -18,14 +18,11 @@ class App {
           $this->DESCRIPTION    = "Little hints and tips for different SME’s to save money on their energy bills."; // No longer than 155 characters.
           $this->DESCRIPTION_LONG = $this->DESCRIPTION; // 200 chars
           // URLS
-          $this->BASEURL        = ""; // This should be set to an absolute value on deployment, for social media linking purposes.
+          $this->BASEURL        =  ""; // "http://switchmybusiness.com/energysavingtips"; // This should be set to an absolute value on deployment, for social media linking purposes.
           $this->SHARE_IMG      = "logo.png"; // For social media
           // Analytics
           $this->SCHEMAROOT     = "WebPage";
-          $this->ANALYTICSCODE  = "xx"; // Replace this
           // Social Media
-          $this->GOOGLE_PROFILE_AUTHOR = ""; // +XYZ
-          $this->GOOGLE_PAGE_PROFILE = ""; // +XYZ
           $this->TWITTER_PUBLISHER = ""; // @XYZ
           $this->TWITTER_AUTHOR = ""; // @XYZ
 
@@ -68,55 +65,26 @@ class App {
           <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
           <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Mass of favicon data, courtesy of the http://faviconit.com/ generator -->
-          <link rel="apple-touch-icon-precomposed" sizes="57x57" href="fav/apple-touch-icon-57x57.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="fav/apple-touch-icon-114x114.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="fav/apple-touch-icon-72x72.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="fav/apple-touch-icon-144x144.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="60x60" href="fav/apple-touch-icon-60x60.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="120x120" href="fav/apple-touch-icon-120x120.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="76x76" href="fav/apple-touch-icon-76x76.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="152x152" href="fav/apple-touch-icon-152x152.png" />
-          <link rel="icon" type="image/png" href="fav/favicon-196x196.png" sizes="196x196" />
-          <link rel="icon" type="image/png" href="fav/favicon-96x96.png" sizes="96x96" />
-          <link rel="icon" type="image/png" href="fav/favicon-32x32.png" sizes="32x32" />
-          <link rel="icon" type="image/png" href="fav/favicon-16x16.png" sizes="16x16" />
-          <link rel="icon" type="image/png" href="fav/favicon-128.png" sizes="128x128" />
-          <meta name="msapplication-TileColor" content="#FFFFFF" />
-          <meta name="msapplication-TileImage" content="mstile-144x144.png" />
-          <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
-          <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
-          <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
-          <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+          <!-- Metadata -->
+          <title>{$this->DESCRIPTION}</title>
+          <meta itemprop="name" content="{$this->PROJECTNAME}">
+          <meta itemprop="description" content="{$this->DESCRIPTION}" name="description">
+          <meta itemprop="image" content="{$this->IMGURL}social.png" name="image">
 
-      <!-- Metadata
-      Test these with:
-        - https://cards-dev.twitter.com/validator
-        - https://developers.facebook.com/tools/debug
-        - http://www.google.com/webmasters/tools/richsnippets
-        - https://developers.pinterest.com/rich_pins/validator/
-      -->
-          <title>{$this->PAGETITLE}</title>
-          <meta itemprop="name" content="{$this->PROJECTNAME}" />
-          <meta itemprop="description" content="{$this->DESCRIPTION}" name="description"/>
-          <meta itemprop="image" content="{$this->SHARE_IMG}" />
-
-          <!-- \\ Twitter Card -->
+          <!-- Social \ Twitter Card -->
           <meta name="twitter:title" content="{$this->PAGETITLE}">
-          <meta name="twitter:site" content="{$this->BASEURL}">
-          <meta name="twitter:card" content="summary_large_image">
-          <meta name="twitter:image:src" content="{$this->SHARE_IMG}"> <!-- at least 280x150px -->
-          <meta name="twitter:description" content="{$this->DESCRIPTION_LONG}">
-          <meta name="twitter:site" content="{$this->TWITTER_PUBLISHER}"> <!-- @publisher_handle -->
-          <meta name="twitter:creator" content="{$this->TWITTER_AUTHOR}"> <!-- @author_handle -->
+          <meta name="twitter:image" content="{$this->IMGURL}social.png">
+          <meta name="twitter:description" content="{$this->DESCRIPTION}">
+          <meta name="twitter:site" content="{$this->TWITTER_PUBLISHER}">
+          <meta name="twitter:creator" content="{$this->TWITTER_AUTHOR}">
 
-        <!-- \\ Opengraph/Facebook -->
+          <!-- Social \ Opengraph/Facebook -->
           <meta property="og:title" content="{$this->PAGETITLE}" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="{$this->BASEURL}"/>
-          <meta property="og:site_name" content="{$this->PAGETITLE}" />
-          <meta property="og:description" content="{$this->DESCRIPTION_LONG}" />
-          <meta property="og:image" content="{$this->SHARE_IMG}" />
+          <meta property="og:site_name" content="{$this->PUBLISHER}" />
+          <meta property="og:description" content="{$this->DESCRIPTION}" />
+          <meta property="og:image" content="{$this->IMGURL}social.png" />
+          <meta property="og:type" content="article" />
+          <meta property="og:url" content="{$this->BASEURL}" />
 
       <!-- Font Icon set: font awesome from BootstrapCDN
           Example: https://fortawesome.github.io/Font-Awesome/examples/
@@ -130,6 +98,8 @@ class App {
 
       </head>
       <body {$this->BODYINJECT}>
+      <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54af9d6678f1771e" async="async"></script>
+
       <!-- ===============
       BEGIN CONTENT VIEW
       ================ -->
@@ -137,7 +107,7 @@ HTML;
   }
 
   /*==============================================*==============================================*==============================================
-  | Document Foot template, include through $app->document_head();
+  | Document Foot template, include through $this->document_head();
   */
 
   public function document_foot() { echo <<<HTML
@@ -151,8 +121,7 @@ HTML;
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', '{$this->ANALYTICSCODE}', 'auto');
-        ga('require', 'linkid', 'linkid.js');
+        ga('create', 'UA-55295226-1', 'auto');
         ga('send', 'pageview');
       </script>
     </body>
